5a5be3551881528d34c31031b4107acf5ff31b88
Vinny Allegra <vinnyallegra@gmail.com>
2022-09-28 15:05:56 -0400

    Working Version Before considering weight

--- derive.py
+++ derive.py
@@ -1,8 +1,35 @@
+from ast import Str
 import sys, getopt
+   
 
-def find_combinations(cfgDict, len):
+def find_combinations(cfgDict, length):
+   comboSet = set()
+   worklist = [list(cfgDict.keys())[0]]
+   while worklist:
+      stringToModify = worklist.pop(0)
+      curlen = len(stringToModify)
+      if curlen > length:
+         continue
+      containsAllTerminal = True
+      for key in cfgDict:
+         if(key in stringToModify):
+            containsAllTerminal = False
+            for value in cfgDict.get(key):
+               if(value != key):
+                  worklist.append(stringToModify.replace(key, value, 1))
+      if curlen == length:
+         if(containsAllTerminal):
+            comboSet.add(stringToModify) 
+   setList = list(comboSet)
+   setList.sort()
+   for string in setList:
+      for char in string:
+         print(char + " ", end = ' ')
+      print(' ')
+   
 
-   print()
+
+   
 
 
 def create_Dictionary(inputfile):
@@ -32,7 +59,7 @@ def main(argv):
       exit(1)
    for opt, arg in opts:
       if opt in ['-l', '--length']:
-         length = arg  
+         length = int(arg)
       else:
          print("Invalid Argument")
          exit(1)
