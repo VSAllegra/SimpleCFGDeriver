ac7a7450c20dc27d19f4612dd514e94bd2cd3fbc
Vinny Allegra <vinnyallegra@gmail.com>
2022-09-28 16:01:40 -0400

    Edited to Work with CFGs containing terminals and nonterminals that are longer in length thatn 1

--- derive.py
+++ derive.py
@@ -1,13 +1,16 @@
 from ast import Str
 import sys, getopt
-   
+
+def getWeight(string):
+   weight = len((string.split(' ')))
+   return weight
 
 def find_combinations(cfgDict, length):
    comboSet = set()
    worklist = [list(cfgDict.keys())[0]]
    while worklist:
       stringToModify = worklist.pop(0)
-      curlen = len(stringToModify)
+      curlen = getWeight(stringToModify)
       if curlen > length:
          continue
       containsAllTerminal = True
@@ -15,43 +18,33 @@ def find_combinations(cfgDict, length):
          if(key in stringToModify):
             containsAllTerminal = False
             for value in cfgDict.get(key):
-               if(value != key):
-                  worklist.append(stringToModify.replace(key, value, 1))
+                  if(value != key):
+                     worklist.append(stringToModify.replace(key, value, 1))
       if curlen == length:
          if(containsAllTerminal):
             comboSet.add(stringToModify) 
    setList = list(comboSet)
    setList.sort()
-   for string in setList:
-      for char in string:
-         print(char + " ", end = ' ')
-      print(' ')
-   
-
-
-   
-
-
+   for combo in setList:
+      print(combo)
+ 
 def create_Dictionary(inputfile):
    retDict = dict()
    for line in open(inputfile, 'r'):
       lineArray = line.split()
       key = lineArray[0]
+      if not (key in retDict):
+         retDict[key] = []
       valueToAdd = ""
       for string in lineArray[2:]:
-         valueToAdd += string
-      if key in retDict:
-         retDict[key] = [retDict[key], valueToAdd]
-      else:
-         retDict[key] = valueToAdd
-      
-
+         valueToAdd += string + ' '
+      valueToAdd = valueToAdd[:-1]
+      retDict[key].append(valueToAdd)
    return retDict
 
 def main(argv):
    inputfile = ''
    length = 3
-   steps = 0
    try:
       opts, args = getopt.getopt(argv, "l:", ["length =", "file"])
    except: 
@@ -66,6 +59,7 @@ def main(argv):
    inputfile = args[0]
    cfgDict = create_Dictionary(inputfile)
    find_combinations(cfgDict, length)
+
    
 
 
